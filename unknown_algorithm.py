63bda3be786919ebdcae2b7956b904ddf30d1afa
Aditya Sahni <sahnia3@mcmaster.ca>
2024-04-06 21:17:36 -0400

    yo

---
+++ unknown_algorithm.py
@@ -0,0 +1,56 @@
+import random
+
+class DirectedWeightedGraph:
+
+    def __init__(self):
+        self.adj = {}
+        self.weights = {}
+
+    def are_connected(self, node1, node2):
+        for neighbour in self.adj[node1]:
+            if neighbour == node2:
+                return True
+        return False
+
+    def adjacent_nodes(self, node):
+        return self.adj[node]
+
+    def add_node(self, node):
+        self.adj[node] = []
+
+    def add_edge(self, node1, node2, weight):
+        if node2 not in self.adj[node1]:
+            self.adj[node1].append(node2)
+        self.weights[(node1, node2)] = weight
+
+    def w(self, node1, node2):
+        if self.are_connected(node1, node2):
+            return self.weights[(node1, node2)]
+
+    def number_of_nodes(self):
+        return len(self.adj)
+
+def init_d(G):
+    n = G.number_of_nodes()
+    d = [[float("inf") for j in range(n)] for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if G.are_connected(i, j):
+                d[i][j] = G.w(i, j)
+        d[i][i] = 0
+    return d
+
+#Assumes G represents its nodes as integers 0,1,...,(n-1)
+def unknown(G):
+    n = G.number_of_nodes()
+    d = init_d(G)
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if d[i][j] > d[i][k] + d[k][j]: 
+                    d[i][j] = d[i][k] + d[k][j]
+    return d
+
+
+
+#changes nigga
